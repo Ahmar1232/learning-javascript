0156cb8050f8da5e75f832e0c480eae36773cd57
Ahmar1232 <139193399+Ahmar1232@users.noreply.github.com>
2023-07-12 00:25:24 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+let  emailId = "ahmar@google.com"
+var accountPassword = "60265" // obselete because of scope issues(block and functional scope)
+userCountry = "pakistan"; //not good practice to declare a variable
+let userCity;
+console.log(accountId);
+console.table([accountId,emailId,userCountry,userCity])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code in this file as newer version
+//alert ("hlo") // we are using nodejs, not browser
+console.log(typeof 3 
+    +
+     3); // not a good practice, since code readability should be our first priority
+console.log( typeof "hlo")
+let state=null; // null is used to represent empty(nothing) value i.e., null is a standalone value
+let  userCountry;
+let isLoggedIn=false
+console.log(typeof state); //object 
+console.log(typeof userCountry); //undefined
+console.log(typeof isLoggedIn);
+console.log(null); 
+console.log(undefined); 
